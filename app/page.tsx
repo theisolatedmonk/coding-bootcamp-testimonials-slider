a0e83d1e1f6cff47bf1df0241ba51054d9c1ad4f
theisolatedmonk <96rahulseth@gmail.com>
2023-07-21 22:06:44 +0530

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,23 @@ import patternCurve from "@/images/pattern-curve.svg";
 import patternQuotes from "@/images/pattern-quotes.svg";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex">
+        <div className="flex flex-col">
+          <p>
+            “ I’ve been interested in coding for a while but never taken the
+            jump, until now. I couldn’t recommend this course enough. I’m now in
+            the job of my dreams and so excited about the future. ”{" "}
+          </p>
+          <p>
+            {" "}
+            Tanya Sinclair <span>UX Engineer</span>
+          </p>
+        </div>
+        <div className="">
+          <Image src={AvatarTanya} alt={""}></Image>
+        </div>
+      </div>
+    </main>
   );
 }
